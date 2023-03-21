e38228359da355d260b237ab148c0606e6d84a04
Mikhail Bei <m.bei@neusta.de>
2023-03-21 10:22:46 +0100

    feat(vanilla): test Thread.sleep() and interrupt

---
+++ src/main/java/com/example/threads/ThreadVanilla.java
@@ -0,0 +1,32 @@
+package com.example.threads;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ThreadVanilla {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new TestSleep());
+        thread.start();
+        Thread.sleep(1000);
+        thread.interrupt();
+    }
+
+}
+
+@Slf4j
+class TestSleep implements Runnable{
+    @Override
+    public void run() {
+        log.info("start");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ignored) {
+            log.info("exception");
+        }
+        log.info(String.valueOf(Thread.currentThread().isInterrupted()));       //when thread sleep interrupt is false
+        log.info("end");
+    }
+}
